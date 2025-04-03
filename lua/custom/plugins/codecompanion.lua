10d24c2e583c00be4fe3e6e97aafc7d090590197
kaufon <kauanfvale@gmail.com>
2025-04-03 19:07:36 -0300

    add jarvis a.i

---
+++ lua/custom/plugins/codecompanion.lua
@@ -0,0 +1,35 @@
+return {
+  "olimorris/codecompanion.nvim",
+  config = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  event = "VeryLazy",
+  opts = {
+    copilot = function()
+      return require("codecompanion.adapters").extend("copilot", {
+        schema = {
+          model = {
+            default = "claude-3.7-sonnet",
+          },
+        },
+      })
+    end,
+    strategies = {
+      chat = {
+        roles = {
+          user = "K.V",
+          llm = "Jarvis A.I"
+        }
+      }
+    },
+    display = {
+      chat = {
+        intro_message = "Welcome home sir! I am Jarvis A.I, your personal assistant. How can I help you today?",
+      }
+    }
+
+  },
+
+}
