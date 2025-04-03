78d5c9edbd8b6eacff0fba49d270a163a22cba0f
kaufon <kauanfvale@gmail.com>
2025-04-02 22:44:19 -0300

    change keyamp for copilot

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -2,7 +2,7 @@ return {
   "github/copilot.vim",
   event = "VeryLazy",
   config = function()
-    vim.keymap.set('i', '<C-D>', 'copilot#Accept("\\<CR>")', {
+    vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
       expr = true,
       replace_keycodes = false
     })
