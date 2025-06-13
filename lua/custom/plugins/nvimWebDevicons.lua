f64e563a8eddaca502fc562698ba24604c312f0f
kaufon <kauanfvale@gmail.com>
2025-06-13 11:42:14 -0300

    add nestjs icon

--- lua/custom/plugins/nvimWebDevicons.lua
+++ lua/custom/plugins/nvimWebDevicons.lua
@@ -1,8 +1,8 @@
 local nvimWebDevicons = {
   "nvim-tree/nvim-web-devicons",
   config = function()
-    local web_devicons = require("nvim-web-devicons")
-    web_devicons.setup({
+    local web_devicons = require "nvim-web-devicons"
+    web_devicons.setup {
       override = {
         rb = {
           icon = "",
@@ -10,13 +10,28 @@ local nvimWebDevicons = {
           name = "DevIconRb",
         },
         ["nest-cli.json"] = {
-          icon = "",
+          icon = "",
           color = "#e0234e",
           name = "DevIconNestCliJson",
-        }
+        },
       },
-    })
+      -- Add this section for pattern-based matching
+      override_by_extension = {
+        -- This pattern matches any file that ends with ".module.ts"
+        ["module.ts"] = {
+          icon = "",
+          color = "#e0234e",
+          name = "DevIconNestModule", -- A unique name for the new rule
+        },
+        ["controller.ts"] = {
+          icon = "",
+          color = "#e0234e",
+          name = "DevIconNestModule", -- A unique name for the new rule
+        },
+      },
+    }
   end,
   event = "VeryLazy",
 }
+
 return nvimWebDevicons
